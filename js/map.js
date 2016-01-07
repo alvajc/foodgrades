90eeadcb298b43e1312fb68797aff91779e649af
Juan Alvarado <alva.juan79@gmail.com>
2016-01-06 16:19:53 -0800

    Building google maps to display restaurant location

---
+++ js/map.js
@@ -0,0 +1,9 @@
+
+    var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 33.983198 , lng: -118.185804},
+        zoom: 18
+      });
+    }
+    
