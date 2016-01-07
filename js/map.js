fb18c6d95f4ecee1ba7ef97529bcfa34436b050f
Juan Alvarado <alva.juan79@gmail.com>
2016-01-07 13:34:02 -0800

    Deleted files

--- js/map.js
+++
@@ -1,9 +0,0 @@
-
-    var map;
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 33.983198 , lng: -118.185804},
-        zoom: 18
-      });
-    }
-    
