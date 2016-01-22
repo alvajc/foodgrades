d09b8c6bfc45e7014ccffc9353ffe7a63c6c286e
Juan Alvarado <alva.juan79@gmail.com>
2016-01-22 14:22:32 -0800

    Moved all routes into a static middleware

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var yelpApi = require('./js/yelp.js');
 var scoreApi = require('./js/score.js');
+
 app.use('/search', yelpApi);
 app.use('/score', scoreApi);
 app.use('/images', express.static('images'));
-
 app.use(express.static('./'));
 
 app.listen(1337);
